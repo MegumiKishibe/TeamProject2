1e9d9dca4a9369c08b782d33f7b04d21d8f9461b
yuzu <yuzuha.1415s@gmail.com>
2025-12-16 23:21:00 +0900

    初期設定追加

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/example', function () {
+    return view('example');
+});
+
